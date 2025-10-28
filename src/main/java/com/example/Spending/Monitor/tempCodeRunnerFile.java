7dcf97033716526941be39922aaf015b84586f72
shahla <Shahlag@users.noreply.github.com>
2025-10-28 14:19:01 +0530

    Update

---
+++ src/main/java/com/example/Spending/Monitor/tempCodeRunnerFile.java
@@ -0,0 +1,13 @@
+package com.example.Spending.Monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpendingMonitorApplication.class, args);
+	}
+
+}
